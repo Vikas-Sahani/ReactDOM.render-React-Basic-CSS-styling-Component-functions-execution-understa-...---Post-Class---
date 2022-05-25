55cdc1af8a3157895cce507714d20576d805a190
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2022-05-25 12:52:17 +0530

    uploading answer3 with reactDomrender

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    React.createElement(App, null, null), 
+    document.getElementById("root")
+);
